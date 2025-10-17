2d427f5561a417f6c29a080a6357b658b5ad9b97
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2025-10-17 10:44:10 +0900

    Create __init__.py

---
+++ DeepLearningCompetition_submission-tool/__init__.py
@@ -0,0 +1,11 @@
+# submission_tool/__init__.py
+
+"""
+submission_tool: 学生提出用CSVファイルを作成するツール
+"""
+
+from .submission_utils import create_submission_file
+
+__all__ = ["create_submission_file"]
+__version__ = "0.1.0"
+__author__ = "Taiga Masuda"
